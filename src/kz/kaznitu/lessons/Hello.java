b723cdf3c2acef9c3f55935b277c35beb3298246
Aigerym Faizulla <askarovnaa97@gmail.com>
2017-10-02 14:23:02 +0600

    master бұтағында Hello.java файлына тағы озгеристер жасалды.

--- src/kz/kaznitu/lessons/Hello.java
+++ src/kz/kaznitu/lessons/Hello.java
@@ -13,6 +13,9 @@ public class Hello {
 
         System.out.println(MyMath.max3(8,9,12));
 
+        System.out.println(MyMath.isEven(12));
+        System.out.println(MyMath.isEven(7));
+
     }
 }
 
