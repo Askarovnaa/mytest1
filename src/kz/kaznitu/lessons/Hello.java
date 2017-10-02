e9ae51bbb5cb9723cc8d2f006330e741c09f5fa0
Aigerym Faizulla <askarovnaa97@gmail.com>
2017-10-02 11:33:30 +0600

    Коммит

--- src/kz/kaznitu/lessons/Hello.java
+++ src/kz/kaznitu/lessons/Hello.java
@@ -1,4 +1,10 @@
 package kz.kaznitu.lessons;
 
 public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        System.out.println("Бұл зертханалық жұмыстың мақсаты:");
+        System.out.println("GitHub пен жұмыс жасап үйрену");
+    }
 }
+
